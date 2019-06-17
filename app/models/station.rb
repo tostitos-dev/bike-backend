90488893099afbb86ca3b864cd6fa1b1756d28a1
Tostitos <camila@avispa.tech>
2019-06-17 01:44:21 -0400

    hotfix searchable name

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,10 +4,16 @@ class Station < ApplicationRecord
   has_many :telemetries
 
   def update_searchable_name
-    return raw_name unless raw_name.include?('-')
+    return special_searcheable_name unless raw_name.include?('-')
 
     clean_s = raw_name.split(' - ')
     clean_s.slice!(0)
     update name: clean_s.join(' - ')
+  end
+
+  def special_searcheable_name
+    clean_s = raw_name.split(' ')
+    clean_s.slice!(0)
+    update name: clean_s.join(' ')
   end
 end
